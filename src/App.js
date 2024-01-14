e16398cdabfbd3aa688cce3955123f8f5e2d9b83
Nazil1712 <ndhalwala@gmail.com>
2024-01-14 11:07:51 +0530

    Login and SignUp page created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import './App.css';
-import ProductList from './features/product-list/ProductList';
-import Home from './pages/Home';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import "./App.css";
+import ProductList from "./features/product-list/ProductList";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Home></Home>
+      </>
+    ),
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignUpPage />,
+  },
+]);
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     {/* <LoginPage/> */}
+  //     <SignUpPage/>
+  //     {/* <Home></Home> */}
+  //   </div>
+  // );
+
   return (
     <div className="App">
-      <Home></Home>
+      <RouterProvider router={router} />
     </div>
   );
 }
